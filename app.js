f56616d9d1232a2765e17155c483438e3fbdfd46
amulyakoduri <93916645+amulyakoduri@users.noreply.github.com>
2022-07-06 11:26:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,59 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="Heading">Summar Art Camp! 5 Days of Artists and Planning Monet, Van Gogh, Matisse, $ More</h1>
+        <p className="subheading">Multi-Day Course 
+        <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657003081/question-circle_juobez.svg" className='image-size' alt=""/></p>
+        <div className="content-container">
+          <div className='about-card'>
+            <p className='about-container'>In this 5 day class we will explore artists Monet, Matissue, Van Gogh, among others and then recreate painithing using crayon and watercolor. Students will have fun learning about the artisits & creating their own art inspired by their work.</p>
+            <div className='profile-container'>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657004939/christopher-campbell-rDEOVtE7vOs-unsplash_isdidn.jpg" className='profile' alt=""/>
+              <p className='name'>Kimberly R Moseler</p>
+            </div>
+            <div className='card-container'>
+            <div className='star-container'>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <p className='total'>467 total reviews for this teacher</p>
+            </div>
+            <div className='star-container'>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657005556/kisspng-yellow-star-color-clip-art-star-5ab5c2d6b6b2f7.9303585415218613347483_pvvm3v.png" className="star" alt=""/>
+              <p className='total'>5 reviews for the class</p>
+            </div>
+            <p className='learner'>Completed by 21 learners</p>
+            <div className='bottom-container'>
+              <button className='button'>See Class Schedule</button>
+              <div className='love-container'>
+                <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657016711/WhatsApp_Image_2022-07-05_at_3.54.21_PM_tszegu.jpg" className='love' alt=""/>
+                <p>Save</p>
+              </div>
+              <div className='love-container'>
+                <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657015655/Pngtree_right_direction_arrow_icon_4243485_b3mlft.png" className='love' alt=""/>
+                <p>Share</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div className='imag-container'>
+            <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657001695/WhatsApp_Image_2022-07-05_at_11.34.48_AM_ukuyeb.jpg" className='picture' alt=""/>
+            <div className='image-container'>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657002223/WhatsApp_Image_2022-07-05_at_11.39.01_AM_rvp5ad.jpg" className="pictures" alt=""/>
+              <img src="https://res.cloudinary.com/drnjmmqvg/image/upload/v1657002269/WhatsApp_Image_2022-07-05_at_11.39.15_AM_z3ik7r.jpg" className="pictures" alt=""/>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
